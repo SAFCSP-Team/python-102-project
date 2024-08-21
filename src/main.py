de2fc94debf1a087f48ef94de54ef2928d1a99fc
malekcoders <149315149+malekcoders@users.noreply.github.com>
2024-08-21 17:35:13 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,33 @@
 from tkinter import * 
 
-# Create class Person with __init__ method
-    # in __init__ set self.name = name
-    # create set_name() and get_name() functions
-    # create write_file() function
-        # open file tasks.txt in append mode
-        # write self.name to file
+
+# 1- Create class Person with __init__ method wit private __name
+
+    # create variable total = 0
+    
+    # in __init__ set self.__name = __name
+    
+        # private variable name
+        
+        # increment total by 1
+        
+
+    # create set_name() function
+
+    # create get_name() function
+    
+    # create a method write_file() function
+        # open file names.txt in append mode
+            # write self.name to file
         # close file
+    
+    # create a classmethod print_file() 
+        # open file names.txt in read mode
+            # print file name 
+    
 
 tk = Tk()  
+tk.title("Python 102")
 tk.geometry("900x600") # set window size
 
 lab = Label(tk, text="Enter a name") # defined
@@ -17,16 +36,29 @@ lab.pack() # display
 ent = Entry(tk) 
 ent.pack()
 
+# ____ Your code here ____
+# 2- Create function check_inputs()
+
+    # check if ent.get() is empty True then msg.config text = "Error message"
+  
+        
+    # else create object from Task class pass ent.get() as argument 
+
+        # print total 
+        
+        # edit msg using .config to display the name added ex: "Ahmed added" use f-string 
+        
+        # call write_file() method from obj1   
+        
+
 msg = Label(tk, text="") 
 msg.pack() 
 
-# Your code here
-# create function check_inputs()
-    # check if ent.get() is empty True then msg.config text = "Error message"
-    # else create obj1 from Task class pass ent.get() as argument 
-    # call write_file() method from obj1   
-
-btn = Button(tk, text="Save")
+btn = Button(tk, text="Save", command=check_inputs)
 btn.pack()
+
+print("_____________")
+# 3- Call print_file() method from Person class
+
 
 tk.mainloop()
